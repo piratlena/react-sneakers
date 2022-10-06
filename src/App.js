d969e6e3ae88518dddac56a232193b68364c1ba6
Elena Brazhnikova <lenovosibirsk@mail.ru>
2022-10-06 12:12:43 +0700

    start sneakers project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+      <div className="headerLeft">
+      <svg />
+        <div className="headerInfo">
+          <h3>React Sneakers</h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+        <ul className="headerRight">
+          <li><svg/><span>1205 руб.</span></li>
+          <li><svg/></li>
+        </ul>
       </header>
     </div>
   );
